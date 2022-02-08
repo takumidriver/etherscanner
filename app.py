d55d45cd0efdcd646d6677939e94ecee98c6228f
takumidriver <takumi2022@protonmail.com>
2022-02-08 07:00:18 -0800

    displays tx data and current eth price

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import config
+from web3 import Web3
 from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
+w3 = Web3(Web3.HTTPProvider(config.INFURA_URL))
 
 @app.route("/")
 def index():
-    return "etherscanner!"
+    result = requests.get(config.url, headers=config.headers, params=config.payload).json()
+    return render_template("index.html", result=result)
+
+@app.route("/address/<addr>")
+def address(addr):
+    return render_template("address.html", addr=addr)
+
+@app.route("/block/<block_number>")
+def block(block_number):
+    return render_template("block.html", block_number=block_number)
 
 @app.route("/tx/<hash>")
 def transaction(hash):
-    return render_template("transaction.html", hash=hash)
+    transaction = w3.eth.get_transaction(hash)
+    return render_template("transaction.html", hash=hash, transaction=transaction)
--- test.py
+++ test.py
@@ -5,10 +5,11 @@ w3 = Web3(Web3.HTTPProvider(config.INFURA_URL))
 
 print(w3.eth.block_number)
 
-balance = w3.eth.get_balance('0x15F2636c01d0F43f1F04D22361b8D8d02D27669A')
+#random address from etherscan
+balance = w3.eth.get_balance('0xE21413BC0d8CC4c757745A4AeF7Ef74a8805B3E8')
 print(balance)
 ether_balance = w3.fromWei(balance, 'ether')
 print(ether_balance)
-
+#random tx from etherscan
 transaction = w3.eth.get_transaction('0x66ecac3f18d32b1efbd3603d8582eb6228a7a20b6a6875014c89db7f24cee74c')
 print(transaction)
