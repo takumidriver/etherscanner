6012c0e9520ecc33e80997b95af0d6dc69c49896
takumidriver <takumi2022@protonmail.com>
2022-03-05 19:29:04 -0800

    fix formatting

--- price.py
+++ price.py
@@ -1,5 +1,3 @@
-
-
 import asyncio, sys
 
 from pythclient.pythaccounts import PythPriceAccount, PythPriceStatus
@@ -7,7 +5,6 @@ from pythclient.solana import SolanaClient, SolanaPublicKey, SOLANA_DEVNET_HTTP_
 
 
 async def get_price():
-    # devnet DOGE/USD price account key (available on pyth.network website)
     account_key = SolanaPublicKey("HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J")
     solana_client = SolanaClient(endpoint=SOLANA_DEVNET_HTTP_ENDPOINT, ws_endpoint=SOLANA_DEVNET_WS_ENDPOINT)
     price: PythPriceAccount = PythPriceAccount(account_key, solana_client)
@@ -16,7 +13,6 @@ async def get_price():
 
     price_status = price.aggregate_price_status
     if price_status == PythPriceStatus.TRADING:
-        # Sample output: "DOGE/USD is 0.141455 ± 7.4e-05"
         return {
             'price': price.aggregate_price,
             'confidence': price.aggregate_price_confidence_interval
